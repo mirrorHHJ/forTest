a3d7751fcb3c4719a3c511cf4e157ecc0aaa7461
mirrorHHJ <526282331@qq.com>
2020-10-20 21:12:32 +0800

    mir

--- src/components/tab/tab.vue
+++ src/components/tab/tab.vue
@@ -18,7 +18,7 @@
         @scroll="onScroll"
         :options="slideOptions">
         <cube-slide-item v-for="(tab,index) in tabs" :key="index">
-          <component :is="tab.component" :data="tab.data"></component>
+          <component :is="tab.component" :data="tab.data" ref="component"></component>
         </cube-slide-item>
       </cube-slide>
     </div>
@@ -63,9 +63,14 @@
         }
       }
     },
+    mounted() {
+      this.onChange(this.index)
+    },
     methods: {
       onChange(current) {
         this.index = current
+        const component = this.$refs.component[current]
+        component.fetch && component.fetch()
       },
       onScroll(pos) {
         const tabBarWidth = this.$refs.tabBar.$el.clientWidth
